ef27fb6c4c5986bb44ab3a8e50c27eee34a62959
Jiguro <phamhaotri@googlemail.com>
2025-08-03 15:16:05 +0200

    Allow refresh token to log back in

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,8 @@ createRoot(document.getElementById('root')).render(
   <Auth0Provider
     domain = {import.meta.env.VITE_AUTH0_DOMAIN}
     clientId = {import.meta.env.VITE_AUTH0_CLIENT_ID}
+    useRefreshTokens = {true}
+    cacheLocation="memory"
     authorizationParams = {{
       redirect_uri: window.location.origin
     }}
